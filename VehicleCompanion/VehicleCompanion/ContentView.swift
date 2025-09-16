df7dc2784992018c1d2c122c83e2c271aefb9549
Yordan Kenarov <yokenarov@icloud.com>
2025-09-16 14:07:59 +0300

    Initial project setup

---
+++ VehicleCompanion/VehicleCompanion/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  VehicleCompanion
+//
+//  Created by Jordan on 16/09/2025.
+//
+
+import SwiftData
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+
+    var body: some View {
+        VStack {
+        }
+    }
+}
---
+++ VehicleCompanion/VehicleCompanion/VehicleCompanionApp.swift
@@ -0,0 +1,32 @@
+//
+//  VehicleCompanionApp.swift
+//  VehicleCompanion
+//
+//  Created by Jordan on 16/09/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VehicleCompanionApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
