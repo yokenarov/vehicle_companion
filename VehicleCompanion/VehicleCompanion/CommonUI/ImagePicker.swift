2a4729658af2ec3fee672a9df95aa3f7faaf27cf
Yordan Kenarov <yokenarov@icloud.com>
2025-09-18 16:46:16 +0300

    Add ImagePickerView

---
+++ VehicleCompanion/VehicleCompanion/CommonUI/ImagePicker.swift
@@ -0,0 +1,37 @@
+//
+//  ImagePicker.swift
+//  VehicleCompanion
+//
+//  Created by Jordan on 18/09/2025.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        init(_ parent: ImagePicker) { self.parent = parent }
+
+        func imagePickerController(_ picker: UIImagePickerController,
+                                   didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            picker.dismiss(animated: true)
+        }
+    }
+}
